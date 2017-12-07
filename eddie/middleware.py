3d1f04c4560fb0fbb4db4efbc7cb5c7d38e93621
rom.cohen90@gmail.com <rom.cohen90@gmail.com>
2017-12-07 01:45:46 -0500

    Added middleware user validation

--- eddie/middleware.py
+++ eddie/middleware.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-
+from eddie.db_client import *
 import falcon
 
 global EXEMPT_ROUTES, EXEMPT_METHODS
@@ -20,8 +20,6 @@ class AuthMiddleware(object):
     """
 
     def process_request(self, req, resp):
-        token = req.get_header('Authorization')
-
         for exempt_path in EXEMPT_ROUTES:
             if exempt_path in req.path:
                 return
@@ -30,8 +28,24 @@ class AuthMiddleware(object):
             if req.method == exempt_method:
                 return
 
-        if token is None or not self._token_is_valid():
-            raise falcon.HTTPUnauthorized(description='Auth token required')
+        if self._is_user_valid(req):
+            raise falcon.HTTPUnauthorized(description='valid user required')
 
-    def _token_is_valid(self):
-        return(True) # You should do this better
+    def _is_user_valid(self, req):
+        """
+        Checks if the given request is from a valid user, i.e the user's id is in the rider or driver database
+        :param req: The given request object
+        :return: True iff the given request is from a valid user
+        """
+        # Check if rider
+        user_id = req.get_header('id')
+        rdb_response = rdb.db('eddie').table('riders').get(user_id).run(rdb_conn)
+        if len(rdb_response) > 0:
+            return True # Rider found, reroute to rider endpoints
+
+        # Check if driver
+        rdb_response = rdb.db('eddie').table('drivers').get(user_id).run(rdb_conn)
+        if len(rdb_response) > 0:
+            return True # Driver found, reroute to rider endpoints
+
+        return False
