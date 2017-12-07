bd9974043866636e8c47c9da66675dc43dc1ea2f
David Hachuel <dhachuel@gmail.com>
2017-12-06 23:02:17 -0500

    Added Travis CI config file and one sample unit test

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,9 +1,15 @@
+import sys, os
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
+sys.path.append(os.path.dirname(__location__))
+print(os.path.dirname(__location__))
 import falcon
 from falcon import testing
-import msgpack
 import pytest
+import datetime
 
 from eddie.app import api
+from eddie.helpers import logisticMap, getReQLNow
 
 
 @pytest.fixture
@@ -11,18 +17,35 @@ def client():
     return testing.TestClient(api)
 
 
+def test_helpers():
+    assert getReQLNow().date() == datetime.datetime.now().date()
 
-def test_list_images(client):
-    doc = {
-        'images': [
-            {
-                'href': '/images/1eaf6ef1-7f2d-4ecc-a8d5-6e8adba7cc0e.png'
-            }
-        ]
-    }
 
-    response = client.simulate_get('/quotes')
-    result_doc = msgpack.unpackb(response.content, encoding='utf-8')
 
-    assert result_doc == doc
-    assert response.status == falcon.HTTP_OK
+# def test_get_quote(client):
+#     doc = {
+#         "created": "2017-12-07 03:19:32.014000+00:00",
+#         "dropoff_location": {
+#             "latitude": 40.759155474313346,
+#             "longitude": -73.95326614379883
+#         },
+#         "id": "a825b041-ed6b-4b66-a96d-dcfdb1488a89",
+#         "pickup_datetime": "2017-11-16 15:42:49.440762-05:00",
+#         "pickup_location": {
+#             "latitude": 40.7246704,
+#             "longitude": -73.9918311
+#         },
+#         "quote": {
+#             "double-necker": 46.82,
+#             "kramer": 34.71,
+#             "ukelele": 18.72
+#         },
+#         "rider_id": "58d7875b-40f1-4cab-b650-bfc972bb8a46"
+#     }
+#
+#     response = client.simulate_get('/quotes/a825b041-ed6b-4b66-a96d-dcfdb1488a89')
+#     result_doc = response.content
+#
+#
+#     assert result_doc == doc
+#     assert response.status == falcon.HTTP_OK
